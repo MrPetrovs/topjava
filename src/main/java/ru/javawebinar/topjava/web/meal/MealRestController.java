4c4946e5116805a73bf5a9b4aa040ce902935c03
MrPetrovs <smokik1@gmail.com>
2021-06-14 21:30:31 +0100

    hw2 add layers to project

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,10 +1,38 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
-import javax.servlet.http.HttpServlet;
+import java.util.Collection;
 
-public class MealRestController extends HttpServlet {
+@Controller
+public class MealRestController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
     private MealService service;
 
+    public Meal save(int userId, Meal meal) {
+        log.info(meal.isNew() ? "Create {}" : "Update {}", meal);
+        return service.save(userId, meal);
+    }
+
+    public boolean delete(int userId, int id) {
+        log.info("Delete {}", id);
+        return service.delete(userId, id);
+    }
+
+    public Meal get(int userId, int id) {
+        log.info("Get {}", id);
+        return service.get(userId, id);
+    }
+
+    public Collection<Meal> getAll(int userId) {
+        log.info("getAll");
+        return service.getAll(userId);
+    }
 }
